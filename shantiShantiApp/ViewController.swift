87ec441c793bf215d4038dfdc1b1591f0dd7a339
Elizabeth Brown <ec.brown33@gmail.com>
2021-02-22 13:11:31 -0800

    We have background and quotes generating correctly.

--- shantiShantiApp/ViewController.swift
+++ shantiShantiApp/ViewController.swift
@@ -6,15 +6,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var shantiBackground: UIImageView!
+    @IBOutlet weak var shantiSaying: UILabel!
+    var tapCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tapTapTap(_ sender: Any) {
+        tapCount = tapCount + 1
+          print ("click")
+          let shantiWisdom=["Hi Beautiful","Today brings joy","Say Shanti"]
+          
+          let randomIndex = Int(arc4random_uniform(UInt32(shantiWisdom.count)))
+          
+          // Get a random Saying
+          
+          let randomSaying = shantiWisdom[randomIndex]
+          shantiSaying.text = randomSaying
+    }
+    
+      /* */
+    
+    
 }
 
