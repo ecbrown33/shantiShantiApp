b6c6243595b25f83d0b43616e6d18f05341d9a67
Elizabeth Brown <ec.brown33@gmail.com>
2021-02-22 12:13:10 -0800

    test

--- shantiShantiApp/ViewController.swift
+++ shantiShantiApp/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var shantiBackground: UIImageView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
